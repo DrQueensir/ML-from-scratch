3bd808dced82ea55c3a32f5c6f5c39a91d297528
Divyam Agarwal <divyam.agarwal0202@gmail.com>
2026-07-05 14:06:14 +0530

    linear regression batch

---
+++ algorithms/linear_models/linear_regression_batch.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+class LinearRegression:
+    def __init__(self, learningrate=0.01,iterations=1000):
+        self.weights=None
+        self.bias=None
+
+        self.learningrate=learningrate
+        self.iterations=iterations
+
+    def fit(self,X,Y):
+        samples,features=X.shape
+        self.weights=np.zeros(features)
+        self.bias=0
+
+        for _ in range(self.iterations):
+            y_pred=np.dot(X,self.weights)+self.bias
+            y_error=y_pred-Y
+            dw=(1/samples)*np.dot(X.T,y_error)
+            db=(1/samples)*np.sum(y_error)
+            self.weights=self.weights - (self.learningrate*dw)
+            self.bias=self.bias - (self.learningrate*db)
+
+
+
+
+    def predict(self,X):
+        return np.dot(X,self.weights)+self.bias
