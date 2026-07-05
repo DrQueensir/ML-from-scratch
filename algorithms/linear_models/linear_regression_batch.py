59f35bc304e2dd89926d80f55b843287f44933db
Divyam Agarwal <divyam.agarwal0202@gmail.com>
2026-07-06 01:08:16 +0530

    cost history done

--- algorithms/linear_models/linear_regression_batch.py
+++ algorithms/linear_models/linear_regression_batch.py
@@ -8,6 +8,8 @@ class LinearRegression:
         self.learningrate=learningrate
         self.iterations=iterations
 
+        self.cost_history=[]
+
     def fit(self,X,Y):
         samples,features=X.shape
         self.weights=np.zeros(features)
@@ -16,8 +18,12 @@ class LinearRegression:
         for _ in range(self.iterations):
             y_pred=np.dot(X,self.weights)+self.bias
             y_error=y_pred-Y
+            cost = (1 / (2 * samples)) * np.sum(y_error ** 2)
+            self.cost_history.append(cost)
+
             dw=(1/samples)*np.dot(X.T,y_error)
             db=(1/samples)*np.sum(y_error)
+
             self.weights=self.weights - (self.learningrate*dw)
             self.bias=self.bias - (self.learningrate*db)
 
