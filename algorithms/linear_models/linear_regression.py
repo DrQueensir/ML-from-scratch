3f8b867a5ea773b06d30418b08d5d76f7df85305
Divyam Agarwal <divyam.agarwal0202@gmail.com>
2026-07-06 23:45:27 +0530

    batch and stochastic

--- algorithms/linear_models/linear_regression.py
+++ algorithms/linear_models/linear_regression.py
@@ -1,20 +1,33 @@
 import numpy as np 
 
 class LinearRegression:
-    def __init__(self, learningrate=0.01,iterations=1000,optimizer="batch"):
+    def __init__(self, learningrate=0.01,iterations=1000,optimizer="batch",batch_size=32):
         self.weights=None
         self.bias=None
 
         self.learningrate=learningrate
         self.iterations=iterations
         self.optimizer=optimizer
+        self.batch_size=batch_size
 
         self.cost_history=[]
 
     def fit(self,X,Y):
-        samples,features=X.shape
+        features=X.shape[1]
         self.weights=np.zeros(features)
         self.bias=0
+
+        if self.optimizer=="batch":
+            self._batch_gradient_descent(X,Y)
+        elif self.optimizer=="sgd":
+            self._stochastic_gradient_descent(X,Y)
+        elif self.optimizer=="mini-batch":
+            self._mini_batch_gradient_descent(X,Y)
+        else:
+            raise ValueError("Unknown optimizer")
+    
+    def _batch_gradient_descent(self,X,Y):
+        samples=X.shape[0]
 
         for _ in range(self.iterations):
             y_pred=np.dot(X,self.weights)+self.bias
@@ -27,9 +40,33 @@ class LinearRegression:
 
             self.weights=self.weights - (self.learningrate*dw)
             self.bias=self.bias - (self.learningrate*db)
+    
+    def _stochastic_gradient_descent(self,X,Y):
+        samples=X.shape[0]
+        for _ in range(self.iterations):
+            for i in range(samples):
+                x_i=X[i]
+                y_i=Y[i]
+                y_pred=np.dot(x_i,self.weights)+self.bias
+                y_error=y_pred-y_i
 
+                dw=x_i*y_error
+                db=y_error
 
+                self.weights=self.weights - (self.learningrate*dw)
+                self.bias=self.bias - (self.learningrate*db)
+            
+            prediction=np.dot(X,self.weights)+self.bias
+            error=prediction-Y
+            cost = (1 / (2 * samples)) * np.sum(error ** 2)
+            self.cost_history.append(cost)
+    
+    def _mini_batch_gradient_descent(self,X,Y):
+        samples=X.shape[0]
 
+        for _ in range(self.iterations):
+            for _ in range(self.batch_size):
+                
 
     def predict(self,X):
         return np.dot(X,self.weights)+self.bias
