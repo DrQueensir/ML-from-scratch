b5c4fc6b5bf63b38fbc119ab93910a06a8db66cd
Divyam Agarwal <divyam.agarwal0202@gmail.com>
2026-07-13 00:14:38 +0530

    ridge regression done

---
+++ algorithms/linear_models/lasso_regression.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+class LassoRegression:
+
+    def __init__(self):
+        pass
+
+    def fit(self,X,Y):
+        pass
+
+    def predict(self,X):
+        pass
---
+++ algorithms/linear_models/ridge_regression.py
@@ -0,0 +1,36 @@
+import numpy as np 
+
+class RidgeRegression:
+
+    def __init__(self,learningrate=0.01,iterations=1000,lambda_=1.0):
+        self.weights=None
+        self.bias=None
+
+        self.learningrate=learningrate
+        self.iterations=iterations
+        self.lambda_=lambda_
+
+        self.cost_history=[]
+
+    def fit(self,X,Y):
+        samples,features=X.shape
+        self.weights=np.zeros(features)
+        self.bias=0
+
+        for _ in range(self.iterations):
+            y_pred=np.dot(X,self.weights)+self.bias
+            y_error=y_pred-Y
+
+            cost=((1/2*samples)*np.sum(y_error**2))+((self.lambda_/(2*samples))*np.sum(self.weights**2))
+            self.cost_history.append(cost)
+
+            dw=(1/samples)*np.dot(X.T,y_error)+((self.lambda_/samples)*self.weights)
+            db=(1/samples)*np.sum(y_error)
+
+            self.weights=self.weights- self.learningrate*dw
+            self.bias=self.bias- self.learningrate*db
+
+    def predict(self,X):
+        return np.dot(X,self.weights)+self.bias
+
+
