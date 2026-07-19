319674bcd7aae790e9461a6b96cb3e40848bc86e
Divyam Agarwal <divyam.agarwal0202@gmail.com>
2026-07-19 23:54:07 +0530

    gradient boosting started

---
+++ algorithms/trees/gradient_boosting.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class GradientBoosting:
+
+    def __init__(self):
+        pass
+
+    def fit(self,X,Y):
+        pass
+
+    def predict(self,X):
+        pass
+
+    def score(self,X,Y):
+        pass
